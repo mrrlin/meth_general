02fb6f8184464e1d281977e6315b8cdcab70e844
Angelina <angelina.kulyaeva@yandex.ru>
2024-01-13 15:03:51 +0300

    Input + p

---
+++ js/index.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const input = document.createElement('input');
+const p = document.createElement('p');
+
+input.type = 'text';
+
+document.body.append(input, p);
+
+const showText = () => p.textContent = input.value;
+
+let timeout;
+
+const debounce = () => {
+  clearTimeout(timeout);
+  timeout = setTimeout(showText, 300);
+};
+
+input.addEventListener('keyup', debounce);
